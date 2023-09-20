a0d84668ebef588597aab26a424ee0ebcf0a5991
Shelf <mohamedshelfwork@gmail.com>
2023-09-20 15:31:40 +0300

    Fixed icons issue with TopBar Component

--- src/component/TopBar.jsx
+++ src/component/TopBar.jsx
@@ -1,4 +1,7 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot, faPhoneVolume } from '@fortawesome/free-solid-svg-icons'
+
 export const TopBar = () => {
   return (
     <header className="container-fluid bg-primary text-white d-none d-lg-flex">
@@ -8,9 +11,9 @@ export const TopBar = () => {
             <h2 className="text-white fw-bold m-0">Garag KSP</h2>
           </a>
           <div className="ms-auto d-flex align-items-center">
-            <small className="ms-4"><i className="fa fa-map-marker-alt me-3" aria-hidden="true"></i>123 Street, New York, USA</small>
+            <small className="ms-4"><FontAwesomeIcon icon={faLocationDot} /> 123 Street, New York, USA</small>
             <small className="ms-4"><i className="fa fa-envelope me-3" aria-hidden="true"></i>info@example.com</small>
-            <small className="ms-4"><i className="fa fa-phone-alt me-3" aria-hidden="true"></i>+012 345 67890</small>
+            <small className="ms-4"><FontAwesomeIcon icon={faPhoneVolume} /> +012 345 67890</small>
             <div className="ms-3 d-flex">
               <a className="btn btn-sm-square btn-light text-primary rounded-circle ms-2 d-flex align-items-center justify-content-center" href="#!" aria-label="Facebook"><i
                 className="fab fa-facebook-f" aria-hidden="true"></i></a>
@@ -25,3 +28,4 @@ export const TopBar = () => {
     </header>
   );
 }
+
