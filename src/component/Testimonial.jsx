fee1bfa621059639fc73fb5c052c66cb5f075694
Shelf <mohamedshelfwork@gmail.com>
2023-10-30 18:22:37 +0200

    Integrated Testimonials with back-end

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -1,8 +1,3 @@
-import testimonialPersonImage1 from '../assets/img/testimonial-1.jpg'
-import testimonialPersonImage2 from '../assets/img/testimonial-2.jpg'
-import testimonialPersonImage3 from '../assets/img/testimonial-3.jpg'
-import testimonialPersonImage4 from '../assets/img/testimonial-4.jpg'
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCards } from 'swiper/modules';
 
@@ -10,8 +5,9 @@ import { EffectCards } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../ThemeContext';
+import instance from '../../axiosConfig';
 
 // TestimonialItem component
 const TestimonialItem = ({ imageSrc, altText, content, clientName, profession }) => {
@@ -19,72 +15,65 @@ const TestimonialItem = ({ imageSrc, altText, content, clientName, profession })
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
   return (
     <div className={`testimonial-item rounded p-4 p-lg-5 mb-5 d-flex flex-column justify-content-center justify-content-lg-start bg-white ${darkMode && 'bg-secondary'}`}>
-      <img className="mb-4" src={imageSrc} alt={altText} />
-      <p className="mb-4 text-lg-start text-primary fw-light">{content}</p>
-      <h5 className="text-lg-start text-dark">{clientName}</h5>
-      <span className="text-primary text-lg-start text-purple">{profession}</span>
+      {imageSrc && <img className="testemonail__image mb-4" src={imageSrc} alt={altText} />}
+      {content && <p style={{ fontSize: '80%' }} className="mb-4 text-lg-start text-primary fw-light">{content}</p>}
+      {clientName && <h5 className="text-lg-start text-dark">{clientName}</h5>}
+      {profession && <span className="text-primary text-lg-start text-purple">{profession}</span>}
     </div>
   )
 };
 
 // Testimonial section component
-export const Testimonial = () => (
-  <div className="container lg-p-4 d-flex justify-content-around align-items-center flex-column flex-lg-row">
-    {/* Testimonial Header */}
-    <div
-      className="text-center text-md-start pb-5 pb-md-0 wow fadeInUp"
-      data-wow-delay="0.1s"
-      style={{ maxWidth: '500px' }}
-    >
-      <p className="fs-5 fw-medium text-primary">Testimonial</p>
-      <h1 className="display-5 mb-5">What Clients Say About Our Services!</h1>
-    </div>
+export const Testimonial = () => {
 
-    {/* Testimonial Cards */}
-    <div className="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
-      <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    instance.get('/users/testimonials')
+      .then(response => {
+        setData(response.data.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+
+  return (
+    <div className="container lg-p-4 d-flex justify-content-around align-items-center flex-column flex-lg-row">
+      {/* Testimonial Header */}
+      <div
+        className="text-center text-md-start pb-5 pb-md-0 wow fadeInUp"
+        data-wow-delay="0.1s"
+        style={{ maxWidth: '500px' }}
       >
-        <SwiperSlide>
-          <TestimonialItem
-            imageSrc={testimonialPersonImage1}
-            altText="Client 1"
-            content="Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo"
-            clientName="Client Name"
-            profession="Profession"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <TestimonialItem
-            imageSrc={testimonialPersonImage2}
-            altText="Client 2"
-            content="Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo"
-            clientName="Client Name"
-            profession="Profession"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <TestimonialItem
-            imageSrc={testimonialPersonImage3}
-            altText="Client 3"
-            content="Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo"
-            clientName="Client Name"
-            profession="Profession"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <TestimonialItem
-            imageSrc={testimonialPersonImage4}
-            altText="Client 4"
-            content="Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo"
-            clientName="Client Name"
-            profession="Profession"
-          />
-        </SwiperSlide>
-      </Swiper>
+        <p className="fs-5 fw-medium text-primary">Testimonial</p>
+        <h1 className="display-5 mb-5">What Clients Say About Our Services!</h1>
+      </div>
+
+      {/* Testimonial Cards */}
+      <div className="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
+        <Swiper
+          effect={'cards'}
+          grabCursor={true}
+          modules={[EffectCards]}
+          className="mySwiper"
+        >
+          {
+            data.map(item => (
+              <SwiperSlide key={item.id}>
+                <TestimonialItem
+                  imageSrc={item.image}
+                  altText={`${item.user_id.name} photo`}
+                  content={item.content}
+                  clientName={item.user_id.name}
+                />
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
     </div>
-  </div>
-);
+  );
+}
