3a43ce5928dfcdf545da266418ca6c062c0dc03e
Shelf <mohamedshelfwork@gmail.com>
2023-11-05 15:59:42 +0200

    Added show more feature to Brands section

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -3,18 +3,33 @@ import { Feature } from '../component'
 import { ThemeContext } from '../ThemeContext'
 import instance from '../../axiosConfig'
 import Carousel from 'react-multi-carousel'
+import { Button, Modal } from 'react-bootstrap'
 
 export const Features = () => {
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
 
-
   const [data, setData] = useState([]);
+
+  /* Modal Logic Start */
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const shouldPaginate = data.length > 3;
+  /* Modal Logic End */
+
 
   useEffect(() => {
     // Fetch data when the component mounts
     instance.get('/public/brands')
       .then(response => {
         setData(response.data.data);
+        // For testing the pagination
+        // setData(Array(5).fill(response.data.data[0]));
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -43,13 +58,14 @@ export const Features = () => {
   return (
     <div className="container-xxl py-5">
       <div className="container">
-        <div className={`text-center mx-auto wow fadeInUp`} data-wow-delay="0.1s" style={{ maxWidth: '500px', marginBottom: '3rem' }}>
+        <div className={`text-center mx-auto wow fadeInUp d-flex ${shouldPaginate ? 'justify-content-between' : 'justify-content-center'}`} data-wow-delay="0.1s" style={{ marginBottom: '3rem' }}>
           <p className={`fs-2 fw-medium text-primary`}>Brands</p>
+          {shouldPaginate && <button className={`btn btn-outline-info btn-sm rounded border-0 fw-light`} onClick={openModal}>Show More</button>}
         </div>
 
         <div className={`${darkMode && 'bg-dark'} row g-0 d-flex gap-4 justify-content-center`} style={{ height: '400px' }}>
           {
-            data.length > 3 ?
+            shouldPaginate ?
               (
                 <Carousel responsive={responsive} infinite={true} showDots={true} autoPlaySpeed={4000} autoPlay={true} keyBoardControl={true}>
                   {
@@ -73,6 +89,36 @@ export const Features = () => {
               ))
           }
         </div>
+
+        {/* JSX Modal Start */}
+        <Modal show={showModal} onHide={closeModal} size="xl">
+          <Modal.Header closeButton>
+            <Modal.Title>Brands</Modal.Title>
+          </Modal.Header>
+          <Modal.Body style={{ height: '80vh' }}>
+            {data && (
+              <div className="d-flex justify-content-center align-items-center flex-wrap gap-3 mt-5">
+                {
+                  data.map(item => (
+                    <Feature
+                      key={item.id}
+                      icon={item.image}
+                      title={item.name}
+                      style={{ width: '100%', maxWidth: '350px' }}
+                    />
+                  ))
+                }
+              </div>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {/* JSX Modal End */}
+
       </div>
     </div >
   );
