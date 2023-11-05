90c1da455d2c0883f2f424595f77691a95c30ab5
Shelf <mohamedshelfwork@gmail.com>
2023-11-05 20:59:38 +0200

    Fixed layout issue for Right-Navigation in darkMoode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './App.css'
 import { Footer, NavigationBar, Copyright, HeaderCarousel, Features, About, Services, Projects, Quote, Team, Testimonial, ProgressBar, Blogs } from './component'
 import { Outlet, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpLong, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { ThemeContext } from './ThemeContext';
 
 function App() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -36,6 +37,8 @@ function App() {
     </div>
   )
 
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+
   return (
     <>
 
@@ -56,12 +59,12 @@ function App() {
 
         {/* To Top Icons Button */}
         <a href="tel:+201096787085" className="progress-bar-item circle" style={{ fontSize: '2.5rem', width: '50px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', fontWeight: 'lighter' }}>
-          <FontAwesomeIcon icon={faPhone} style={{ fontWeight: 'lighter', fontSize: '1.3rem' }} />
+          <FontAwesomeIcon icon={faPhone} style={{ fontWeight: 'lighter', fontSize: '1.3rem' }} color={darkMode ? 'white' : '#333'} />
         </a>
 
         {/* To Top Icons Button */}
         <a href="#section-1" className="progress-bar-item circle" style={{ fontSize: '2.5rem', width: '50px', height: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', fontWeight: 'lighter' }}>
-          <FontAwesomeIcon icon={faArrowUpLong} style={{ fontWeight: 'lighter', fontSize: '1.5rem' }} />
+          <FontAwesomeIcon icon={faArrowUpLong} style={{ fontWeight: 'lighter', fontSize: '1.5rem' }}  color={darkMode ? 'white' : '#333'}/>
         </a>
       </div >
 
