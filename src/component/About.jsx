0728ba623a43c83c0a4f59c2fcd7b9e653df33be
Shelf <mohamedshelfwork@gmail.com>
2023-11-14 00:50:04 +0200

    Fixed About.jsx

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,7 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../ThemeContext';
-import icon5 from '../assets/img/icon/icon-5.png'
-import icon6 from '../assets/img/icon/icon-6.png'
 import instance from '../../axiosConfig';
 
 export const About = () => {
@@ -22,7 +20,8 @@ export const About = () => {
 
   return (
     <>
-      <div className={`container-xxl my-5`} style={{ background: `linear-gradient(rgba(0, 0, 0, .1), rgba(0, 0, 0, .1)), url(${data.image}) center no-repeat`, }}>
+      <div className={`container-xxl my-5`} style={{ position: 'relative' }}>
+        <img src={data.image} style={{ position: 'absolute', inset: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: -1 }} />
         <div className={`container`}>
           <div className={`row g-0`}>
 
@@ -39,24 +38,6 @@ export const About = () => {
                 <p className={`fs-5 fw-medium text-primary`}>About Us</p>
                 <h1 className={`display-6 mb-4`}>{data.name}</h1>
                 <p className={`mb-4`}>{data.description}</p>
-                <div className={`row g-5 pt-2 mb-5`}>
-
-                  {/*
-                  <div className={`col-sm-6 d-flex flex-column align-items-center align-items-lg-start`}>
-                    <img className={`img-fluid mb-4`} src={icon5} alt="Managed Services Icon" />
-                    <h5 className={`mb-3 text-lg-start`}>Managed Services</h5>
-                    <span className={`text-lg-start d-block`}>Clita erat ipsum et lorem et sit sed stet lorem</span>
-                  </div>
-
-                  <div className={`col-sm-6 d-flex flex-column align-items-center align-items-lg-start`}>
-                    <img className={`img-fluid mb-4`} src={icon6} alt="Dedicated Experts Icon" />
-                    <h5 className={`mb-3 text-lg-start`}>Dedicated Experts</h5>
-                    <span className={`text-lg-start d-block`}>Clita erat ipsum et lorem et sit sed stet lorem</span>
-                  </div>
-                  */}
-
-                </div>
-
                 <div className={`d-flex justify-content-center justify-content-center`}>
                   <a className={`btn btn-primary rounded-pill py-3 px-5`} href="#">Explore More</a>
                 </div>
@@ -73,7 +54,7 @@ export const About = () => {
         <div className={`modal-dialog`}>
           <div className={`modal-content rounded-0`}>
             <div className={`modal-header`}>
-              <h3 className={`modal-title`} id="exampleModalLabel">Youtube Video</h3>
+              <h3 className={`modal-title`} id="exampleModalLabel">{data.name}</h3>
               <button type="button" className={`btn-close`} data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className={`modal-body`}>
@@ -82,15 +63,6 @@ export const About = () => {
                 <iframe className={`embed-responsive-item`} id="video"
                   width="1550" height="696" src={data.youtube_video_url} title={data.name} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
               </div>
-
-              {/*
-
-              <div className={`ratio ratio-16x9`}>
-                <iframe className={`embed-responsive-item`} id="video" allowFullScreen
-                  allowScriptAccess="always" width="1550" height="696" src={data.youtube_video_url} title={data.name} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-              </div>
-              */}
-
             </div>
           </div>
         </div>
