0321fea64c222b3d5f774a7f46b0a71fc2d65b61
Shelf <mohamedshelfwork@gmail.com>
2023-11-14 21:14:49 +0200

    Implemented translation for About US Section

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../ThemeContext';
 import instance from '../../axiosConfig';
+import i18n from '../../i18n';
+import { t } from 'i18next';
 
 export const About = () => {
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
@@ -20,7 +22,7 @@ export const About = () => {
 
   return (
     <>
-      <div className={`container-xxl my-5`} style={{ position: 'relative' }}>
+      <div className={`container-xxl my-5`} style={{ position: 'relative' }}  dir={`${i18n.language == 'ar' ? 'rtl' : 'ltr'}`}>
         <img src={data.image} style={{ position: 'absolute', inset: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: -1 }} />
         <div className={`container`}>
           <div className={`row g-0`}>
@@ -35,11 +37,11 @@ export const About = () => {
 
             <div className={`col-lg-6 pt-lg-5 wow fadeIn`} data-wow-delay="0.5s">
               <div className={`${darkMode ? 'bg-dark' : 'bg-white'} rounded-top p-5 mt-lg-5`}>
-                <p className={`fs-5 fw-medium text-primary`}>About Us</p>
+                <p className={`fs-5 fw-medium text-primary`}>{t('about.title')}</p>
                 <h1 className={`display-6 mb-4`}>{data.name}</h1>
                 <p className={`mb-4`}>{data.description}</p>
                 <div className={`d-flex justify-content-center justify-content-center`}>
-                  <a className={`btn btn-primary rounded-pill py-3 px-5`} href="#">Explore More</a>
+                  <a className={`btn btn-primary rounded-pill py-3 px-5`} href="#">{t('exploreMore')}</a>
                 </div>
 
               </div>
@@ -54,7 +56,7 @@ export const About = () => {
         <div className={`modal-dialog`}>
           <div className={`modal-content rounded-0`}>
             <div className={`modal-header`}>
-              <h3 className={`modal-title`} id="exampleModalLabel">{data.name}</h3>
+              <h3 className={`modal-title`} id="exampleModalLabel" style={{flex: 1}}>{data.name}</h3>
               <button type="button" className={`btn-close`} data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className={`modal-body`}>
